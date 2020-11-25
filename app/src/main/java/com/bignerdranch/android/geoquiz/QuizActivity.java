646961ea1142ea0b8135cad6eb752b769521d799
xiaozhu-sorce <790362227@qq.com>
2020-11-25 21:52:37 +0800

    GeoQuiz (Challenge1.11)

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
@@ -20,14 +23,18 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_LONG).show();
+                Toast toast = Toast.makeText(QuizActivity.this,R.string.correct_toast, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
         mFalsxeButton=(Button)findViewById(R.id.false_button);
         mFalsxeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+            public void onClick(View vi) {
+                Toast toast = Toast.makeText(QuizActivity.this,R.string.incorrect_toast, LENGTH_LONG);
+                toast.setGravity(Gravity.TOP , 0, 0);
+                toast.show();
             }
         });
     }
