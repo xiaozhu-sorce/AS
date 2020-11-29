3f6e435de522a120afb50093cc4c465e30bc5eb6
xiaozhu-sorce <790362227@qq.com>
2020-11-29 23:17:37 +0800

    GeoQuiz (Chapter 3)

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +16,8 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+    private static final String TAG="QuizActivity";
+    private static final String KEY_INDEX="index";
 
     private Question[] mQuestionBank =new Question[]{
             new Question(R.string.question_australia,true),
@@ -29,7 +32,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onStart() called");
         setContentView(R.layout.activity_quiz);
+
+        if(savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +72,7 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-
+        updateQuestion();
         mPrevButton=(ImageButton)findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,15 +80,56 @@ public class QuizActivity extends AppCompatActivity {
                 if (mCurrentIndex==0)
                     mCurrentIndex=mQuestionBank.length-1;
                 else
-                mCurrentIndex=(mCurrentIndex-1)%mQuestionBank.length;
+                    mCurrentIndex=(mCurrentIndex-1)%mQuestionBank.length;
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart()  called");
+    }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+    private void ButtonEnabled(){
+
     }
     private void updateQuestion(){
         int question=mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
+
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue=mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId=0;
