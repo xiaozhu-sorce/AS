4d5e7b1a8c1be4a6735c137f65c5f4284a300eae
xiaozhu-sorce <790362227@qq.com>
2020-12-06 00:51:03 +0800

    GeoQuiz (Challenge5.5)

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -33,7 +33,9 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia,true,0)//填充数组，使用构造器
     };
     private int mCurrentIndex=0;
-    private boolean mIsCheater;
+    private boolean[] mIsCheater=new boolean[mQuestionBank.length];
+    private static final String SECOND_BUG="Second Bug";
+    private static final String THIRD_BUG="Third Bug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +47,9 @@ public class QuizActivity extends AppCompatActivity {
             mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);//键值结构
             int[] answerList=savedInstanceState.getIntArray(KEY_ANSWER);
             for (int i=0;i<mQuestionBank.length;i++){
-                mQuestionBank[i].setisAnswer(answerList[i]);
+                mQuestionBank[i].setisAnswer(answerList[i]);//把保存在answerList里面的值赋值给mQuestionBank
             }
+            mIsCheater[mCurrentIndex]=savedInstanceState.getBoolean(SECOND_BUG,false);//防止旋转作弊漏洞
         }
 
         mQuestionTextView=(TextView)findViewById(R.id.question_text_view);//引用生成的视图对象，问题数组文字
@@ -91,7 +94,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
-                mIsCheater=false;
+                mIsCheater[mCurrentIndex]=false;
                 updateQuestion();//以上两行用于更新为下一个问题
                 answerLength++;
                 if (answerLength==mQuestionBank.length){//当answerLength长度和问题数量长度一致的时候进行输出
@@ -126,7 +129,7 @@ public class QuizActivity extends AppCompatActivity {
             if (data == null) {
                 return;
             }
-            mIsCheater = CheatActivity.wasAnswerShown(data);
+            mIsCheater[mCurrentIndex] = CheatActivity.wasAnswerShown(data);
         }
     }
 
@@ -159,6 +162,7 @@ public class QuizActivity extends AppCompatActivity {
             answerList[i]=mQuestionBank[i].getisAnswer();//获得每道题的用户的答案
         }
         savedInstanceState.putIntArray(KEY_ANSWER,answerList);
+        savedInstanceState.putBooleanArray(THIRD_BUG,mIsCheater);
     }
 
     @Override
@@ -195,7 +199,7 @@ public class QuizActivity extends AppCompatActivity {
         //通过isAnswertrue方法来获得问题的正确答案，answerIstrue是question构造器中的设置的问题答案的变量;
         int messageResId=0;
 
-        if (mIsCheater){
+        if (mIsCheater[mCurrentIndex]){
             messageResId=R.string.judgement_toast;
         }else {
             if (userPressedTrue==answerIsTrue){
