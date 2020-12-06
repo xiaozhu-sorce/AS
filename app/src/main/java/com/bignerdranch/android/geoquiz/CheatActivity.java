384cccd1359b1ffa2519aea2c566f0b2710f6bad
xiaozhu-sorce <790362227@qq.com>
2020-12-06 23:21:16 +0800

    GeoQuiz (Challenge5.5)

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -10,18 +10,19 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
-    private static final String EXTRA_ANSWER_IS_TRUE = "com.example.testactivity.MainActivity.answer_is_true";
-    private static final String EXTRA_ANSWER_SHOWN = "com.example.testactivity.MainActivity.answer_shown";
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
-    private static final String FIRST_BUG="Firat Bug";
+    private static final String FIRST_BUG="First Bug";
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return intent;
     }
+
     public static boolean wasAnswerShown(Intent result)
     {
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
@@ -32,7 +33,9 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
-        mAnswerTextView = (TextView) findViewById(R.id.show_answer_button);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mShowAnswerButton=(Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
