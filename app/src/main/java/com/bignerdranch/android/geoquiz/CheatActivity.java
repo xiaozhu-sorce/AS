dc9fbe1240f8da76cce6ace11971dd4f81f68ed0
xiaozhu-sorce <790362227@qq.com>
2020-12-07 13:43:54 +0800

    GeoQuiz (Chapter 6)

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
@@ -37,6 +42,7 @@ public class CheatActivity extends AppCompatActivity {
 
         mShowAnswerButton=(Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
@@ -45,6 +51,19 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShowResult(true);
+                int cx = mShowAnswerButton.getWidth()/2;
+                int cy = mShowAnswerButton.getHeight()/2;
+                float radius = mShowAnswerButton.getWidth();
+                Animator anim = ViewAnimationUtils
+                        .createCircularReveal(mShowAnswerButton,cx,cy,radius,0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation){
+                        super.onAnimationEnd(animation);
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+                        anim.start();
             }
         });
         if (savedInstanceState!=null){
